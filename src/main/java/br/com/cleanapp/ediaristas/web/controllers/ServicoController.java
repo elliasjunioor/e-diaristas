297363f77a8c434cdfa5443a3dfe5acdfd0b55d8
elliasjunioor <junior_elias18@hotmail.com>
2023-05-31 17:16:45 -0300

    Iniciando o cadastro de diariastas

--- src/main/java/br/com/cleanapp/ediaristas/web/controllers/ServicoController.java
+++ src/main/java/br/com/cleanapp/ediaristas/web/controllers/ServicoController.java
@@ -1,8 +1,13 @@
 package br.com.cleanapp.ediaristas.web.controllers;
 
+import br.com.cleanapp.ediaristas.core.enums.Icone;
 import br.com.cleanapp.ediaristas.core.models.Servico;
+import br.com.cleanapp.ediaristas.core.repositories.ServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,11 +15,24 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admin/servicos")
 public class ServicoController {
 
-    @GetMapping("cadastrar")
+    @Autowired
+    private ServicoRepository servicoRepository;
+
+    @GetMapping("/cadastrar")
     public ModelAndView cadastrar(){
         var modelAndView = new ModelAndView("admin/servico/form");
         modelAndView.addObject("servico", new Servico());
-
         return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String newCadastro(Servico servico){
+        servicoRepository.save(servico);
+        return "redirect:/admin/servicos/cadastrar";
+    }
+
+    @ModelAttribute("icones")
+    public Icone[] getIcones(){
+        return Icone.values();
     }
 }
